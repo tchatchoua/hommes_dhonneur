1e0f33958091f468d4570e3736ab33eafb4ebc9a
Francis Njiki (VirtueTech) <francis.njiki@jeragm.com>
2025-12-18 21:06:03 +0000

    feat: added capacity

---
+++ frontend/android/app/src/main/java/com/leshommes/dhonneur/MainActivity.java
@@ -0,0 +1,5 @@
+package com.leshommes.dhonneur;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ frontend/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.leshommes.dhonneur',
+  appName: "Les Hommes D'honneur",
+  webDir: 'dist/community-finance-tracker/browser'
+};
+
+export default config;
